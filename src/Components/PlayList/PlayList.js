a8cb39fa850fe2d7516cafdfd8fdf33d6e83cd4d
ylvare <ylva.rehnberg@gmail.com>
2018-07-25 14:41:54 +0200

    =playListInputField

--- src/Components/PlayList/PlayList.js
+++ src/Components/PlayList/PlayList.js
@@ -3,10 +3,25 @@ import './PlayList.css';
 import TrackList from '../TrackList/TrackList'
 
 class PlayList extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      playListName : ''
+    }
+    this.handleInputValueChange = this.handleInputValueChange.bind(this)
+  }
+
+  handleInputValueChange(e){
+    this.setState ({
+      playListName: e.target.value
+    })
+    e.preventDefault()
+  }
   render() {
     return (
       <div className="Playlist">
-            <input defaultValue='New Playlist' />
+            <input onChange={this.handleInputValueChange} defaultValue='New Playlist' />
             <TrackList trackList = {this.props.playList} trackAction = "-" trackFunction = {this.props.removeFromPlayList}/>
             <a className="Playlist-save">SAVE TO SPOTIFY</a>
         </div>
